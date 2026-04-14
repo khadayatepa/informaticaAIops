5228868d891af6290870fca36c4c765adb577fef
khadayatepa <81963189+khadayatepa@users.noreply.github.com>
2026-04-14 21:24:21 +0530

    Update informatica_ai_dashboard_streamlit_git_hub_ready.py

--- informatica_ai_dashboard_streamlit_git_hub_ready.py
+++ informatica_ai_dashboard_streamlit_git_hub_ready.py
@@ -1,13 +1,3 @@
-# ==============================
-# requirements.txt
-# ==============================
-streamlit
-pandas
-openai
-
-# ==============================
-# app.py
-# ==============================
 import streamlit as st
 import pandas as pd
 import os
